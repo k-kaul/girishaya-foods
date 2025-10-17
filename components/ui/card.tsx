e4a20d826635b546ffe6df954b2164656bcb7f07
Kartik <kartik3344@gmail.com>
2025-10-17 18:13:36 +0530

    fixed types

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,17 +2,16 @@ import addToCart from "@/lib/addToCart";
 import { Card } from "flowbite-react";
 import Link from "next/link";
 
-interface Product {
-    productName:string,
-    weight:string,
-    price:number,
-    productId: string,
-}
-
 export function ProductCard({
-    product
+    productName,
+    weight,
+    price,
+    productId
 }:{
-    product:Product;
+    productName?:string;
+    weight?:string;
+    price?:number;
+    productId: string;
 }) {
   return (
     <Card
@@ -22,16 +21,16 @@ export function ProductCard({
     >
       <a href="#">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          {product.productName}
+          {productName}
         </h5>
       </a>
-      <div><span className="text-3xl font-bold text-gray-900 dark:text-white">{product.weight}</span></div>
+      <div><span className="text-3xl font-bold text-gray-900 dark:text-white">{weight}</span></div>
       <div className="flex items-center justify-between">
-        <span className="text-3xl font-bold text-gray-900 dark:text-white">{product.price}</span>
+        <span className="text-3xl font-bold text-gray-900 dark:text-white">{price}</span>
         <Link
           href="#"
           className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-          onClick={() => addToCart({product})}
+          onClick={() => addToCart({productId})}
         >
           <span>Add to cart</span>
         </Link>
