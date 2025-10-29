68bec5b811be11b8dac3305553f9ada8f1d8eb44
Kartik <kartik3344@gmail.com>
2025-10-29 19:10:57 +0530

    chnages in  root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Header from "@/components/common/header";
+import NavBar from "@/components/common/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Header />
+          <NavBar />
           {children}
         </body>
       </html>
