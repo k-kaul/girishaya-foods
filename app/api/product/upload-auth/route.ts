0f597d2b9d802e3ef5b4387d5f2b4bf6a2c956ce
Kartik <kartik3344@gmail.com>
2025-11-12 17:15:44 +0530

    added imagekit file upload logic and FE component

---
+++ app/api/product/upload-auth/route.ts
@@ -0,0 +1,23 @@
+import { currentUser } from "@clerk/nextjs/server"
+import { getUploadAuthParams } from "@imagekit/next/server"
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const user = await currentUser();
+
+    if(!user){
+        return NextResponse.json({
+            success: false,
+            message: "User not signed in"
+        })
+    }
+
+    const { token, expire, signature } = getUploadAuthParams({
+        privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, 
+        publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+        // expire: 30 * 60, // Optional, controls the expiry time of the token in seconds, maximum 1 hour in the future
+        // token: "random-token", // Optional, a unique token for request
+    })
+
+    return Response.json({ token, expire, signature, publicKey: process.env.IMAGEKIT_PUBLIC_KEY })
+}
