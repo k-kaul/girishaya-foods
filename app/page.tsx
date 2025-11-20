8a0fe2ac51d389b444bc93327e977c87fc44e7e6
Kartik <kartik3344@gmail.com>
2025-11-20 18:27:47 +0530

    layout fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HomeProducts from "@/components/home/HomeProducts";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col m-10">
       <HomeProducts />
       <FeaturedProduct />
     </div>
